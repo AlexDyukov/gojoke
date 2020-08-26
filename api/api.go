85e7963eed667c3e9e9f7ccc6659f830ba4eac0a
AlexDyukov <21895986+AlexDyukov@users.noreply.github.com>
2020-08-26 23:01:37 +0300

    Add API skeleton methods (#4)
    Co-authored-by: Alex Dyukov <alex.dyukov.93@gmail.com>

---
+++ api/api.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/alexdyukov/gojoke/db"
+)
+
+func Generate(jdb db.JokeDatabase, w http.ResponseWriter, r *http.Request) {
+}
+func List(jdb db.JokeDatabase, w http.ResponseWriter, r *http.Request) {
+}
+func Get(jdb db.JokeDatabase, w http.ResponseWriter, r *http.Request) {
+}
+func Update(jdb db.JokeDatabase, w http.ResponseWriter, r *http.Request) {
+}
+func Delete(jdb db.JokeDatabase, w http.ResponseWriter, r *http.Request) {
+}
